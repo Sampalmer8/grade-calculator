408391a3df398c18f3c2b91a094c29ac5b99d267
Sampalmer8 <sampalmerg@gmail.com>
2022-09-19 10:58:57 -0600

    java half done

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // JavaScript Code
+
+// Input
+let grade1 = +prompt("Enter your Client-Side Scripting 1 grade:");
+let grade2 = +prompt("Enter your Structured Programming 1 grade:");
+let grade3 = +prompt("Enter your Structured Programming 2 grade:");
+let grade4 = +prompt("Enter your Client-Side Scripting 2 grade:");
+let grade5 = +prompt("Enter your Project A grade:");
+
+// Process
+let finalGrade = (grade1 + grade2 + grade3 + grade4 + grade5)/5;
+
+// Output
+alert(`Your final grade is ${finalGrade}%`);
