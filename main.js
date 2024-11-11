f5d85812694257853fbea2a91a6d9eea17f0e081
Sampalmer8 <113128589+Sampalmer8@users.noreply.github.com>
2024-11-10 16:23:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 // JavaScript Code
 
-
 // Event Listener on the Button
 document.getElementById("btn").addEventListener("click", btnClicked);
 
-
 function btnClicked() {
-    // Input
-    let grade1 = +document.getElementById("clientSideScripting1").value;
-    let grade2 = +document.getElementById("structedProgramming1").value;
-    let grade3 = +document.getElementById("structuredProgramming2").value;
-    let grade4 = +document.getElementById("clientSideScripting2").value;
-    let grade5 = +document.getElementById("projectA").value;
+  // Input
+  let grade1 = +document.getElementById("clientSideScripting1").value;
+  let grade2 = +document.getElementById("structedProgramming1").value;
+  let grade3 = +document.getElementById("structuredProgramming2").value;
+  let grade4 = +document.getElementById("clientSideScripting2").value;
+  let grade5 = +document.getElementById("projectA").value;
 
-    // Process
-    let finalGrade = (grade1 + grade2 + grade3 + grade4 + grade5)/5;
+  // Process
+  let finalGrade = (grade1 + grade2 + grade3 + grade4 + grade5) / 5;
 
-    // Output
+  // Output
 
-    document.getElementById("output").innerHTML = finalGrade.toFixed(0);
+  document.getElementById("output").innerHTML = finalGrade.toFixed(1);
 }
